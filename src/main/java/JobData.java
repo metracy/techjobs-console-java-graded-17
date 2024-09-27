486a700f2e25fb091a6bf3e7241396b63cf48d56
MET <matthew.etracy@gmail.com>
2024-09-26 20:51:34 -0500

    Test was crashing due to gradle needing input yes into console/terminal. Now doesn't hang on the test

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -6,7 +6,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -70,7 +70,7 @@ public class TechJobs {
         }
     }
 
-    // ﻿Returns the key of the selected item from the choices Dictionary
+    // Returns the key of the selected item from the choices Dictionary
     private static String getUserSelection(String menuHeader, HashMap<String, String> choices) {
 
         int choiceIdx = -1;
